7fecb130e8e081f17517ff1fd0baadf14f2e6ea3
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2024-05-26 00:26:52 +0800

    add 5 method

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -18,4 +18,36 @@ class EmployeesController extends Controller
         $employees->delete();
         return redirect()->route('employees.index')->with('success', 'Data pegawai berjaya dihapuskan');
     }
+    public function show($id)
+    {
+        $employees = Employees::find($id);
+        return view('employees.show', compact('employees'));
+    }
+    public function store(Request $request)
+    {
+        $employees = new Employees;
+        $employees->employee_id = $request->employee_id;
+        $employees->first_name = $request->first_name;
+        $employees->last_name = $request->last_name;
+        $employees->gender = $request->gender;
+        $employees->date_of_issue = $request->date_of_issue;
+        $employees->save();
+        return redirect()->route('employees.index')->with('success', 'Data pegawai berjaya ditambah');
+    }
+    public function edit($id)
+    {
+        $employees = Employees::find($id);
+        return view('employees.edit', compact('employees'));
+    }
+    public function update(Request $request, $id)
+    {
+        $employees = Employees::find($id);
+        $employees->employee_id = $request->employee_id;
+        $employees->first_name = $request->first_name;
+        $employees->last_name = $request->last_name;
+        $employees->gender = $request->gender;
+        $employees->date_of_issue = $request->date_of_issue;
+        $employees->save();
+        return redirect()->route('employees.index')->with('success', 'Data pegawai berjaya dikemaskini');
+    }
 }
