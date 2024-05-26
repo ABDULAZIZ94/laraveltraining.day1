72a5d448ef65a86f1fa8d4885c221f41a760bd76
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2024-05-27 00:09:26 +0800

    add vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    @vite(['resources/css/style.css', 'resources/js/app.js'])
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- jQuery AJAX -->
@@ -15,15 +16,14 @@
     <!-- Bootstrap JS -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
+
 <body>
     <div class="container">
         @yield('content')
     </div>
 
-@yield('scripts')
+    @yield('scripts')
 
 </body>
+
 </html>
-
-
-
